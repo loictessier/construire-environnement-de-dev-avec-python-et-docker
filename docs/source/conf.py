910d261cf3fdefd8e4641f3615a0a33c1871f34e
Thierry Chappuis <thierry@chappuis.io>
2018-11-08 10:50:39 +0100

    Configured sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ language = 'fr'
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -75,7 +75,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,6 +10,8 @@ Welcome to apprendre-docker's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+   installation
+
 
 
 Indices and tables
